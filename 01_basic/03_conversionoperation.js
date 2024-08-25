0000e49b4627b2fbe9a357ffceec4f08707a8061
SURAJ KUMAR <105237554+suraj7549@users.noreply.github.com>
2024-08-25 14:54:12 +0000

    commit

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,26 @@
+//let score="33"
+//console.log(typeof(score));
+//console.log(typeof score)
+
+// let score="33"
+// let isTheNumber=Number(score);
+// console.log(isTheNumber);
+// console.log(typeof score)
+// "33"=>33
+// "333abc"=>NaN
+// Treu=>1,false=>0
+
+
+// let loggedIn=1
+// let booleanIsLoggedIn=Boolean(loggedIn);
+// console.log(booleanIsLoggedIn)
+
+//1=>true
+//0=>false
+// ""=>false
+// "suraj "=>true
+
+// let stribIsThe="33"
+// let stringIs=String(stribIsThe);
+// console.log(stribIsThe);
+// console.log(typeof stribIsThe);
