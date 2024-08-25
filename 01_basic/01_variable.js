dda4f8af76e0a21a3935e93878158ddf016ad222
SURAJ KUMAR <105237554+suraj7549@users.noreply.github.com>
2024-08-25 14:00:45 +0000

    commit

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const idNumber=123;
+let passwordName="suraj";
+let emailId="suraj1a@gmail.com"
+accountId="state bank do india"
+//idNumber=123;
+ passwordName="sachi"
+ emailId="suenj3#"
+ accountId="punjab bank of inida"
+let ss;
+/*
+prefer to not use the var because of block scope and funtion scope*/
+console.log([idNumber,passwordName,emailId,accountId]);
+console.table([idNumber,passwordName,emailId,accountId,ss]);
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,24 @@
+// "use state" treat all code as a newer version
+// alert(3+3); we are using node js not browser
+console.log(2+2);
+console.log("SURAJ KUMAR");
+
+let name="suraj";
+let age=18;
+let islogedIn=false;
+let state;
+
+
+//number ka range=>2 to pow 53
+// BigInt
+// string =>""
+// Boolean=>true/false
+// null=>standalone value
+// undefined
+//symbole=>unque
+
+//object 
+
+
+console.log(typeof("suraj"))
+console.log(typeof(12))
