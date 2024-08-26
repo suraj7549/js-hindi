1965f8887376bb0518ea16325b3b055882c016f1
SURAJ KUMAR <105237554+suraj7549@users.noreply.github.com>
2024-08-26 18:12:00 +0000

    data type

---
+++ 01_basic/datatype.js
@@ -0,0 +1,37 @@
+//Primitive data type
+
+// 7 type
+// string , number,boolean,null undefined,symbole
+
+const score=100;
+const scoreValue=100.3;
+const isLoggedIn=true;
+const outSideTemp=null;
+
+const id=Symbol('123');
+const anotherId=Symbol('123')
+
+console.log(id==anotherId);
+
+const bigNumber=2142343473438943848n;
+
+
+// reference (non primitive) datatype
+
+// array object function
+const heroes=["suraj","sachin","prince","raju"];
+
+let myObj={
+    name:"ritesh",
+    age: 23,
+}
+
+const myFunction=function()
+{
+    console.log("hello jee");
+}
+
+console.log(typeof outSideTemp);
+
+
+
