c1a516b55a2d3a9e3bb1a606f4abae104b88f3ec
SURAJ KUMAR <105237554+suraj7549@users.noreply.github.com>
2024-08-26 15:01:12 +0000

    commit

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -24,3 +24,28 @@
 // let stringIs=String(stribIsThe);
 // console.log(stribIsThe);
 // console.log(typeof stribIsThe);
+
+
+//****************OPERATION*******************
+
+// let value=4;
+// let negValue=-value;
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2+2-2*2);
+// console.log(2+2-2*2/2%2);
+
+
+// let str1="suaj"
+// let str2=" sachin"
+// let str3=str1+str2;
+// console.log(str3)
+
+//  console.log("1"+2);
+//  console.log(1+"2");
+//  console.log(1+2+"1");
+//  console.log("1"+2+2);
+
+ 
---
+++ 01_basic/04_camparation.js
@@ -0,0 +1,20 @@
+console.log(2<1);
+console.log(2>1);
+console.log(2==1);
+
+
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// this strictlly equal
+console.log("2"===1);//here capare two different type of the data type 
+//so this is false
+
+
