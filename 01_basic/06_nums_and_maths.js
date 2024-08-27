b3f4c18c3ce82f99650a49352d866c60bdc618e7
SURAJ KUMAR <105237554+suraj7549@users.noreply.github.com>
2024-08-27 12:53:12 +0000

    numbers and maths

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,40 @@
+// const score=400;
+// console.log(score);
+
+// const balance=new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const number=234.24;
+// console.log(number.toPrecision(2));
+
+// const hund=10000000;
+// console.log(hund.toLocaleString('en-IN'));
+
+//+++++++++++++++++++MATHS+++++++++++++++++++++++++
+// console.log(Math.abs(-4));
+// console.log(Math.round(33.55));
+// console.log(Math.round(3.123));
+
+// console.log(Math.ceil(33.55));
+// console.log(Math.floor(3.123));
+// console.log(Math.min(3,4,5,3,2,1));
+// console.log(Math.max(3,4,5,3,2,1));
+
+
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random());
+//  console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
