4a3536c4ccda643161a20ddcfe485f953e254194
SURAJ KUMAR <105237554+suraj7549@users.noreply.github.com>
2024-08-27 11:40:26 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,36 @@
+// const name="suraj";
+// const nameCount=23;
+
+// ///console.log(nameCount+ name + 2334);   this is the old fasion to
+// console.log(`my name is ${name} and my nameCount is ${nameCount}`);
+
+// const myName= new String('suraj-kumar');
+
+// console.log(myName[0]);
+// console.log(myName.__proto__);
+
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
+
+// console.log(myName.charAt(0));
+
+// console.log(myName.indexOf('j'));
+
+// const newMyName=myName.substring(0,3);
+// console.log(newMyName);
+
+// const anotherString=myName.slice(-8,4);
+// console.log(anotherString);
+
+// const newString ="  suraj  ";
+// console.log(newString);
+// console.log(newString.trim());
+
+
+// const url="https://suraj.com/suraj.jss%20djjfie.com";
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('surajaa'));
+
+
+const myName="suraj";
+console.log(myName.split('-'));
