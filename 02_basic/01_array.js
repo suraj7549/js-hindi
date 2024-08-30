c3242d829498cab29ec2804bb0f380ad1e550eec
SURAJ KUMAR <105237554+suraj7549@users.noreply.github.com>
2024-08-30 13:48:51 +0000

    array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,39 @@
+//  const arr=[1,2,3,4,5,6];
+//  const arr2=[2,3,4,3,2,3];
+//  const arr3=["suraj","kumar"];
+
+//  console.log(arr);
+//  console.log(arr3);
+//  console.log(arr2);
+
+//  const myArr2=new Array(1,2,3,4,5,5);
+//   console.log(myArr2[2]);
+  // console.log(myArr2);
+
+  // myArr2.push(5);
+  //  console.log(myArr2);
+  //  myArr2.push(34);
+  //  console.log(myArr2);
+  //  myArr2.pop();
+  //  myArr2.pop();
+  //  myArr2.pop();
+ // myArr2.unshift(3);
+  // myArr2.shift();
+  // console.log(myArr2);
+  // console.log(myArr2.includes(9));
+  ///aarraaayr methdoe 
+  // const myArr2=new Array(1,2,3,4,5,5);
+
+  // const newArr=myArr2.join();
+  // console.log(myArr2);
+  // console.log(newArr);
+  // console.log(typeof(newArr));
+
+  const myArr2=new Array(1,2,3,4,5,5);
+  console.log(myArr2);
+  const arr4=myArr2.slice(1,3);
+  console.log(arr4);
+
+  const arr5=myArr2.splice(1,3);
+  console.log(arr5);
+  console.log(arr5);
---
+++ 02_basic/02_array.js
@@ -0,0 +1,27 @@
+// const marval_heroes=["suraj","sachin","raju"];
+// const dc_heroes=["prince","ritesh","raj"];
+// marval_heroes.push(dc_heroes);
+// console.log(marval_heroes);
+// console.log(marval_heroes[3][1]);
+
+// const allHeres=marval_heroes.concat(dc_heroes);
+// console.log(allHeres);
+
+// const all_new_heroes=[...marval_heroes,...dc_heroes];
+// console.log(all_new_heroes);
+
+// const newArr=[1,2,3,[3,4,5,5,[5,6,7,],4,45,],4,5,6,6];
+// const the_real_array=newArr.flat(Infinity);
+// console.log(the_real_array);
+
+
+// console.log(Array.isArray("surja"));
+// console.log(Array.from("suraj"));
+// console.log(Array.from({name:"suraj"})); intresting
+
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.of(score1,score2,score3));
+
+
