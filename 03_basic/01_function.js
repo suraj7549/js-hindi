2b3c05b28f93680b9532ff6cd37c210e99360de9
SURAJ KUMAR <105237554+suraj7549@users.noreply.github.com>
2024-09-02 08:21:24 +0000

    function and scope

---
+++ 03_basic/01_function.js
@@ -0,0 +1,82 @@
+// function suraj()
+// {
+//     console.log("s");
+//     console.log("u")
+//     console.log("r");
+//     console.log("a")
+//     console.log("j");
+// }
+
+// suraj();
+
+// function addTwoNumber(Number1,Number2)
+// {
+//      let sol=Number1+Number2;
+//     // return sol;
+
+//          //or
+//         // console.log(sol);
+
+// }
+
+//let ans=addTwoNumber(3,4);
+//console.log(ans);
+
+// function loginUserMassage(username)
+// {
+//     //if(username===undefined)
+//     //or
+//     if(!username)
+//     {
+//         console.log("plese enter the username")
+//         return
+//     }
+//     return `${username} just loggin`
+// }
+
+// function loginUserMassage(username="sam")
+// {
+//     //if(username===undefined)
+//     //or
+//     if(!username)
+//     {
+//         console.log("plese enter the username")
+//         return
+//     }
+//     return `${username} just loggin`
+// }
+
+// console.log(loginUserMassage());
+
+// function calcultateCardPrice(num2,num3,num4,...num1)
+// {
+//     return num1;
+// }
+// console.log(calcultateCardPrice(12,23,34,56,67,78));
+
+// const username={
+//     user: "suraj",
+//     price: 23
+// }
+
+// function useBio(anyObject)
+// {
+//     console.log(`the name of the use is ${anyObject.user} and the prince 
+//         is ${anyObject.price}`)
+// }
+
+// //useBio(username);
+//      //or
+
+//      useBio(
+//         {user:"suraj",price:23}
+//      )
+
+
+const myNewArray=[12,34,56,78];
+
+function findTheValue(Aray){
+  return Aray[2];
+}
+
+console.log(findTheValue(myNewArray));
